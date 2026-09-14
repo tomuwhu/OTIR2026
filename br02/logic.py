344ae7447091b7f812d974c9de2793d2e2519a19
= <=>
2026-09-14 18:57:05 +0200

    kész

--- br02/logic.py
+++ br02/logic.py
@@ -4,6 +4,7 @@ p = 1
 oszpsz = [0, 0]
 b = 0
 j = 19
+Z = H.DIV()
 def f(e):
     global j, b, p
     sz = int(e.target.id)
@@ -19,7 +20,9 @@ def f(e):
         p = 0 if p else 1
         e.target.clear()
         e.target.classList.add("O")
-        print(oszpsz)
+        Z.clear()
+        Z <= [H.DIV(i) for i in oszpsz]
 D <= H.H1("Számjáték")
 l = [[rand(1,30),j] for i, j in enumerate(range(20))]
 D <= H.TABLE(H.TR(H.TD(x[0], id=f"{x[1]}", Class=f"p{x[1] % 2}").bind("click", f) for x in l))
+D <= Z
