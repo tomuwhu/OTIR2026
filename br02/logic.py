7253efeeee697ac77e69f5c2db7bfba6e133a963
= <=>
2026-09-14 18:48:16 +0200

    Számjáték

--- br02/logic.py
+++ br02/logic.py
@@ -1,6 +1,25 @@
 from browser import document as D, html as H
-from random import shuffle as sh
-D <= H.H1("Lottó nyerőszámok")
-l = list(range(1, 91))
-sh(l)
-D <= H.TABLE(H.TR(H.TD(i) for i in sorted(l[0:5])))
+from random import randrange as rand
+p = 1
+oszpsz = [0, 0]
+b = 0
+j = 19
+def f(e):
+    global j, b, p
+    sz = int(e.target.id)
+    ém = False
+    if sz == b:
+        b += 1
+        ém = True
+    if sz == j:
+        j -= 1
+        ém = True
+    if ém:
+        oszpsz[p] += int(e.target.innerHTML)
+        p = 0 if p else 1
+        e.target.clear()
+        e.target.classList.add("O")
+        print(oszpsz)
+D <= H.H1("Számjáték")
+l = [[rand(1,30),j] for i, j in enumerate(range(20))]
+D <= H.TABLE(H.TR(H.TD(x[0], id=f"{x[1]}", Class=f"p{x[1] % 2}").bind("click", f) for x in l))
