374e71b3bded20d448b7b7138703865b9926a8b7
= <=>
2026-09-14 17:44:45 +0200

    Aknakereső

--- br02/logic.py
+++ br02/logic.py
@@ -1,13 +1,27 @@
 from browser import document as D, html as H
-D <= H.H1("Hello World!")
-next = "X"
+from random import shuffle as sh
+D <= H.H1("Aknakereső!")
+n = 15
+m = 10
+asz = 26
+T = [1]*asz + [0]*(n * m - asz)
+sh(T)
+def g(e):
+    e.target <= "Z"
 def f(e):
-    global next
-    next = "X" if next == "O" else "O"
     e.target.clear()
-    e.target <= next
-    e.target.classList.add(next)
-D <= H.TABLE(H.TR(
-    H.TD("").bind("click", f)
-        for i in range(10)
-) for j in range(5))
+    pos = int(e.target.id)
+    if T[pos]:
+        e.target <= "B"
+    else:
+        asz = sum([T[pos + j * n + i] for i,j in [(-1, -1), (-1, 0), (-1, 1), 
+        (0, -1), (0, 1),  
+        (1, -1), (1, 0), (1, 1)]])
+        e.target <= asz
+        e.target.classList.add(f"X{asz}")
+D <= H.TABLE([H.TR(
+    H.TD("", id=f"{j * n + i}")
+        .bind("click", f)
+        .bind("contextmenu", g)
+        for i in range(n)
+) for j in range(m)])
