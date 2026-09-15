7db469439aaea29ac8f8e914dad975a3a542224f
Dr. Németh Tamás <tomuwhu@gmail.com>
2026-09-15 16:18:24 +0200

    Aknakereső újra

--- br02/logic.py
+++ br02/logic.py
@@ -1,28 +1,76 @@
 from browser import document as D, html as H
-from random import randrange as rand
-p = 1
-oszpsz = [0, 0]
-b = 0
-j = 19
-Z = H.DIV()
-def f(e):
-    global j, b, p
-    sz = int(e.target.id)
-    ém = False
-    if sz == b:
-        b += 1
-        ém = True
-    if sz == j:
-        j -= 1
-        ém = True
-    if ém:
-        oszpsz[p] += int(e.target.innerHTML)
-        p = 0 if p else 1
+from browser import document as D, html as H
+from random import shuffle as sh
+D <= H.H1("Aknakereső!")
+ml = set()
+n = 15
+m = 10
+asz = 26
+T = [1]*asz + [0]*(n * m - asz)
+sh(T)
+def g(e):
+    pos = int(e.target.id)
+    if pos not in ml:
+        e.target <= "📍"
+        e.target.classList.add(f"Z")
+        ml.add(pos)
+    else:
         e.target.clear()
-        e.target.classList.add("O")
-        Z.clear()
-        Z <= [H.DIV(i) for i in oszpsz]
-D <= H.H1("Számjáték")
-l = [[rand(1,30),j] for i, j in enumerate(range(20))]
-D <= H.TABLE(H.TR(H.TD(x[0], id=f"{x[1]}", Class=f"p{x[1] % 2}").bind("click", f) for x in l))
-D <= Z
+        ml.remove(pos)
+        e.target.classList.remove(f"Z")
+    e.preventDefault()
+nyitva = set()
+
+def f(e):
+    megnyit(e.target)
+
+
+def megnyit(cella):
+    pos = int(cella.id)
+
+    if pos in nyitva:
+        return
+
+    nyitva.add(pos)
+
+    if T[pos]:
+        cella <= "💣"
+        cella.classList.add("Z")
+        return
+
+    y = pos // n
+    x = pos % n
+
+    asz = 0
+
+    for dy in [-1, 0, 1]:
+        for dx in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+
+            yy = y + dy
+            xx = x + dx
+
+            if 0 <= yy < m and 0 <= xx < n:
+                asz += T[yy * n + xx]
+
+    cella <= asz
+    cella.classList.add(f"X{asz}")
+
+    if asz == 0:
+        for dy in [-1, 0, 1]:
+            for dx in [-1, 0, 1]:
+                if dx == 0 and dy == 0:
+                    continue
+
+                yy = y + dy
+                xx = x + dx
+
+                if 0 <= yy < m and 0 <= xx < n:
+                    megnyit(D[str(yy * n + xx)])
+D <= H.TABLE([H.TR(
+    H.TD("", id=f"{j * n + i}")
+        .bind("click", f)
+        .bind("contextmenu", g)
+        for i in range(n)
+) for j in range(m)])
